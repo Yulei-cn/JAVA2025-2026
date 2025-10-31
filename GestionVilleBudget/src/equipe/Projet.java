221d15a8d4205506cb8ced5502686296868d9910
Yulei-cn <116038178+Yulei-cn@users.noreply.github.com>
2025-10-31 21:01:53 +0100

    start

---
+++ GestionVilleBudget/src/equipe/Projet.java
@@ -0,0 +1,13 @@
+package equipe;
+
+public class Projet {
+    private String titre;
+    private String description;
+    private Secteur secteur;
+    private Integer benefice;  // Integer 而不是 int，可以为 null
+    private Integer coutEconomique;
+    private Integer coutSocial;
+    private Integer coutEnvironnemental;
+    
+    // 构造器、getter、setter...
+}
